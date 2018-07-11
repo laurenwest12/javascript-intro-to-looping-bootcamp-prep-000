f87afaaa2dd5ea4a797309bd9ca9a885efbfedf6
Lauren West <laurenwest12@gmail.com>
2018-07-11 23:46:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
-function forLoop (var i = 0; i < 25; i++){
-  console.log ("I am ${i} strange loops.")
+function forLoop(array){
+  for (var i = 0; i < 25; i++)
+  console.log ("I am ${i} strange loops."
 }
